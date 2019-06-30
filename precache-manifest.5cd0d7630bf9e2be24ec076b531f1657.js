5fae60ebccc57cec7a4d07b7b7ad1fa5158e1698
Пользователь <Sviatoslv>
2019-06-30 12:30:48 +0300

    Updates

---
+++ precache-manifest.5cd0d7630bf9e2be24ec076b531f1657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f638b09ba9b50dc5c58c372687fb4190",
+    "url": "/create_reac_app-demo/index.html"
+  },
+  {
+    "revision": "771102f73027237a9bcb",
+    "url": "/create_reac_app-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/create_reac_app-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "771102f73027237a9bcb",
+    "url": "/create_reac_app-demo/static/js/main.2ba1b1f2.chunk.js"
+  },
+  {
+    "revision": "605b44e6be68e8c58931",
+    "url": "/create_reac_app-demo/static/js/runtime~main.32470ae3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create_reac_app-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
